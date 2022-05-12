ab499cf82d7de49a1f128802de1754e848bfe322
pjonaseq21 <gracjan.chamulati2019@gmail.com>
2022-05-12 15:15:45 +0200

    hello2

--- mailer/urls.py
+++ mailer/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from posts.views import logout, posting
 from .views import sendMail,login
-
+from profiles.views import register_request
 
 urlpatterns = [
-    path('register', sendMail),
     path('login', login),
     path('logout', logout),
     path('posting', posting),
+    path('register', register_request),
 ]
--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
 from .models import *
- 
+ from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 class CreateInForum(ModelForm):
     class Meta:
         model= Post
         fields = "__all__"
+
+
