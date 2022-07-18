d53b655a7231ff0cf45abcdf5f4fc67ca4c762bd
Gracjan <gracjan.chamulati2019@gmail.com>
2022-07-18 12:17:11 +0200

    daily fix

--- mailer/urls.py
+++ mailer/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
-<<<<<<< HEAD
-from posts.views import logout, posting
-=======
 from posts.views import logout, posting,admininfo
->>>>>>> bd9c3a2f (sss)
 from .views import sendMail,login
 from profiles.views import register_request
 
@@ -13,8 +9,5 @@ urlpatterns = [
     path('logout', logout),
     path('posting', posting),
     path('register', register_request),
-<<<<<<< HEAD
-=======
     path('admininfo', admininfo),
->>>>>>> bd9c3a2f (sss)
 ]
